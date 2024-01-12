6c689c90a4a9b351093ce790a9b88191cb9f9e67
KarthikeyanS <karthikeyan.saravanan.carecentra.com>
2024-01-12 16:26:08 +0530

    add config file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from src.logger import logging
 from src.exception import CustomException
+from src.config import CONFIG_FILE_PATH
+from src.utils import read_yaml
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 import traceback
@@ -12,9 +14,13 @@ from pathlib import Path
 
 @dataclass
 class DataIngestionConfig:
-    raw_data_path:str=os.path.join("artifacts\Data","Data.csv")
-    train_data_path:str=os.path.join("artifacts\Data","train.csv")
-    test_data_path:str=os.path.join("artifacts\Data","test.csv")
+    config_values =  read_yaml(CONFIG_FILE_PATH)
+    raw_data_path:str = Path(config_values.data_ingestion.local_data_file)
+    train_data_path:str= Path(config_values.data_ingestion.train_data_file)
+    test_data_path:str= Path(config_values.data_ingestion.test_data_file)
+    remove_cols = list(config_values.data_ingestion.remove_cols)
+    output_mapping = dict(config_values.data_ingestion.output_mapping)
+    test_size = float(config_values.data_ingestion.test_size) 
 
 
 class DataIngestion:
@@ -28,14 +34,13 @@ class DataIngestion:
             df = pd.read_csv(self.ingestion_config.raw_data_path)
             logging.info("\t\t\t Total Shape of Data : {}rows {}columns \n".format(df.shape[0], df.shape[1]))
             logging.info("\t\t\t Total Duplicate records : {} \n".format(df.duplicated().sum()))
-            cols = ["gill-spacing" , "stem-root" , "stem-surface" , "veil-type" , "veil-color" , "spore-print-color","has-ring"]
             df.drop_duplicates(inplace=True)
-            df.drop(columns= cols, inplace= True)
+            df.drop(columns= self.ingestion_config.remove_cols , inplace= True)
             logging.info("\t\t\t After delete & remove the duplicate and unwanted columns the overall shape is {} \n".format(df.shape))
-            df["class"] = df["class"].replace({"p" : 0 , "e" : 1})
+            df["class"] = df["class"].replace(self.ingestion_config.output_mapping)
             df.columns = df.columns.str.replace('-', '_')
             df.rename(columns = {"does_bruise_or_bleed" : "does-bruise-or-bleed"} , inplace = True)
-            train_data , test_data = train_test_split(df , test_size=0.25)
+            train_data , test_data = train_test_split(df , test_size=self.ingestion_config.test_size)
             logging.info("\t\t\t Train Data Shape is : {} Train Data Shape is : {} \n".format(train_data.shape , test_data.shape))
             train_data.to_csv(self.ingestion_config.train_data_path , index = False)
             test_data.to_csv(self.ingestion_config.test_data_path , index = False)
---
+++ src/utils/__init__.py
@@ -0,0 +1,34 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+import json
+from src.logger import logging
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import warnings
+from ensure import ensure_annotations
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
